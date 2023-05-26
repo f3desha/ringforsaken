e4f3642c71e2f3d8d595360a03288c1261947a4f
Oleksandr Kuzmenko <oleksandr.kuzmenko@rocketroute.com>
2023-05-26 22:56:02 +0300

    Added observer example

---
+++ src/DesignPatterns/Behavioral/Observer/UserExample/UserObserver.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace App\DesignPatterns\Behavioral\Observer\UserExample;
+
+use SplSubject;
+
+class UserObserver implements \SplObserver {
+    /**
+     * @var SplSubject[]
+     */
+    private array $changedUsers = [];
+    /**
+     * @inheritDoc
+     */
+    public function update(SplSubject $subject): void {
+        $this->changedUsers[] = clone $subject;
+    }
+
+    /**
+     * @return SplSubject[]
+     */
+    public function getChangedUsers(): array {
+        return $this->changedUsers;
+    }
+
+}
